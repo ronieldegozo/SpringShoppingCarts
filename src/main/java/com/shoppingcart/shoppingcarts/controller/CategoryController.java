bf2532d11aebd83d0b4d7e9ae3dd54c0e76965cb
ronieldegozo <ronieldegozo@gmail.com>
2024-09-16 13:01:07 -0700

    Adding a Rest Crud for CategoryController

--- src/main/java/com/shoppingcart/shoppingcarts/controller/CategoryController.java
+++ src/main/java/com/shoppingcart/shoppingcarts/controller/CategoryController.java
@@ -1,4 +1,94 @@
 package com.shoppingcart.shoppingcarts.controller;
 
+import com.shoppingcart.shoppingcarts.exceptions.AlreadyExistsException;
+import com.shoppingcart.shoppingcarts.exceptions.ResouseNotFoundException;
+import com.shoppingcart.shoppingcarts.model.Category;
+import com.shoppingcart.shoppingcarts.response.ApiResponse;
+import com.shoppingcart.shoppingcarts.service.category.InterfaceCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+
+    private final InterfaceCategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllServices(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return  ResponseEntity.ok(new ApiResponse("Categories Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Can't find Categories", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name){
+        try {
+            Category theCategory = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Category added successfully!", theCategory));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT)
+                    .body(new ApiResponse(e.getMessage(), null));
+
+        }
+    }
+
+    @GetMapping("/{id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById (@PathVariable Long id){
+        try {
+            Category theCategory = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category Id Found!", theCategory));
+        } catch (ResouseNotFoundException e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Can't find Categories Id", e.getMessage()));
+        }
+    }
+
+    @GetMapping("/category/{name}/category")
+    public ResponseEntity<ApiResponse> getCategoryByName (@PathVariable String name){
+        try {
+            Category theCategory = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Category Name Found!", theCategory));
+        } catch (ResouseNotFoundException e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Can't find Categories Name", e.getMessage()));
+        }
+    }
+
+
+    @DeleteMapping("/category/{id}/delete")
+    public ResponseEntity<ApiResponse> deleteCategory (@PathVariable Long id){
+        try {
+            categoryService.deleteCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category Deleted!", null));
+        } catch (ResouseNotFoundException e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(e.getMessage(), e.getMessage()));
+        }
+    }
+
+
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<ApiResponse> updateCategory (@PathVariable Long id, @RequestBody Category category) {
+        try {
+            Category theCategory = categoryService.updateCategory(category, id);
+            return ResponseEntity.ok(new ApiResponse("Category Name Found!", theCategory));
+        } catch (ResouseNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse("Can't find Categories Name", e.getMessage()));
+        }
+    }
+
+
+
 }
