d374fda64872de7933048daf1a85713139e45b50
Mc Roniel De Gozo <63585718+ronieldegozo@users.noreply.github.com>
2024-12-24 07:45:24 +0000

    Test Commit

--- src/main/java/com/shoppingcart/shoppingcarts/ShoppingcartsApplication.java
+++ src/main/java/com/shoppingcart/shoppingcarts/ShoppingcartsApplication.java
@@ -8,6 +8,7 @@ public class ShoppingcartsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingcartsApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
