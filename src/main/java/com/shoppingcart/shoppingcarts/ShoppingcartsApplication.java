bd86451322568ee0d6ac1a1b439657a188bf78b9
Mc Roniel <ronieldegozo@gmail.com>
2024-12-30 17:29:41 +0800

    Test v3

--- src/main/java/com/shoppingcart/shoppingcarts/ShoppingcartsApplication.java
+++ src/main/java/com/shoppingcart/shoppingcarts/ShoppingcartsApplication.java
@@ -8,7 +8,7 @@ public class ShoppingcartsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingcartsApplication.class, args);
-		System.out.println("Test2");
+		System.out.println("Test2 v2");
 	}
 
 }
