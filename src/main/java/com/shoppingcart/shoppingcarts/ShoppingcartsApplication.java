ecadcc4777ab5504bc034c2c0dd184452d30b3c2
Mc Roniel <ronieldegozo@gmail.com>
2024-12-30 17:32:36 +0800

    Remove Testing scenarios

--- src/main/java/com/shoppingcart/shoppingcarts/ShoppingcartsApplication.java
+++ src/main/java/com/shoppingcart/shoppingcarts/ShoppingcartsApplication.java
@@ -8,9 +8,6 @@ public class ShoppingcartsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingcartsApplication.class, args);
-		System.out.println("Test2 v1");
-		System.out.println("Test2 v2");
-		System.out.println("Test2 v3");
 	}
 
 }
